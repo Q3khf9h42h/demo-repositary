a4171545334276060d620e7eb89d031890e98155
harshita sethiya <harshita.sethiya252@gmail.com>
2023-06-13 23:48:26 +0530

    demo commit2

--- demo.js
+++ demo.js
@@ -20,3 +20,37 @@ li[1].style.backgroundcolor='yellow';
 for(var i=0;i<li.length;i++){
 li[i].style.backgroundColor="#f4f4f4";
 }
+//QuerySelector
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+
+var input=document.querySelector('input');
+input.value="Hello world";
+
+var submit=document.querySelector('input[type="submit"]');
+submit.value="SEND"
+
+var item=document.querySelector('.list-group-item');
+item.style.color="red"
+
+var lastitem=document.querySelector('.list-group-item:last-child');
+lastitem.style.color="blue"
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color="coral"
+
+//Queryselectorall//
+
+var title=document.querySelectorAll('.title')
+console.log("titles");
+title[0].textcontent="Hello";
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var odd=document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].styl.backgroundColor="#f4f4f4";
+    even[i].styl.backgroundColor="#ccc";
+}
+
+
